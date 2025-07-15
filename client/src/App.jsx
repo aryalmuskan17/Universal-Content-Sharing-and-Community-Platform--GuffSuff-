84e85bf409c447c540f4da126e779c2074998ba4
Muskan Aryal <aryalmuskan17@gmail.com>
2025-07-15 10:45:54 +0545

    initial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch('/api/hello')
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
-      .catch(err => setMessage("Failed to load message"))
-  }, [])
+    fetch('http://localhost:4000/api/hello')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => {
+        console.error("Failed to fetch from backend:", err);
+        setMessage("Failed to load message");
+      });
+  }, []);
 
   return (
     <>
       <h1>{message}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
